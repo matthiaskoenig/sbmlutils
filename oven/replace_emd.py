1534ff731921f78fef7e9b728a6cf5c9d2a22d16
Matthias Koenig <konigmatt@googlemail.com>
2017-03-24 19:21:30 +0100

    adding latest changes

--- oven/replace_emd.py
+++ oven/replace_emd.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import libsbml
+import warnings
 
 
 def flattenExternalModelDefinitions(doc):
@@ -15,8 +16,7 @@ def flattenExternalModelDefinitions(doc):
     :param doc: SBMLDocument
     :return: SBMLDocument with ExternalModelDefinitions replaced
     """
-    # FIXME: handle /multiple levels of hierarchies. This must be done
-    # recursively to handle the ExternalModelDefinitions of submodels
+    # FIXME: handle /multiple levels of hierarchies recursively (ExternalModelDefinitions of submodels)
 
     model = doc.getModel()
     if model is None:
@@ -24,78 +24,94 @@ def flattenExternalModelDefinitions(doc):
 
     comp_doc = doc.getPlugin("comp")
     emd_list = comp_doc.getListOfExternalModelDefinitions()
+
+    # no ExternalModelDefinitions
     if (emd_list is None) or (len(emd_list) == 0):
-        # no ExternalModelDefinitions
         return doc
-    else:
-        emd_ids = []
-        for emd in emd_list:
-            emd_ids.append(emd.getId())
 
-            # get the model definition from the model
-            ref_model = emd.getReferencedModel()
+    # ExternalModelDefinitions set as ModelDefinitions
+    emd_ids = []
+    mds = []
+    for emd in emd_list:
+        emd_ids.append(emd.getId())
 
-            # --------------------------------------
-            ref_doc = ref_model.getSBMLDocument()
-            print('\n', ref_model)
-            for k in range(ref_doc.getNumPlugins()):
+        # get the model definition from the model
+        ref_model = emd.getReferencedModel()
 
-                plugin = ref_doc.getPlugin(k)
+        # store model definition
+        md = libsbml.ModelDefinition(ref_model)
+        mds.append(md)
 
-                # enable the package on the main SBMLDocument
-                uri = plugin.getURI()
-                prefix = plugin.getPrefix()
-                name = plugin.getPackageName()
-                doc.enablePackage(uri, prefix, True)
-                print(name, plugin)
-                print('uri:', uri)
-                print('prefix:', prefix)
+    packages = []
+    for emd in emd_list:
+        # get the model definition from the model
+        ref_model = emd.getReferencedModel()
+        ref_doc = ref_model.getSBMLDocument()
+        # print('\n', ref_model)
+        for k in range(ref_doc.getNumPlugins()):
 
-                # set the respective required tag (check if already required=True,
-                # to avoid True -> False by later submodels)
-                ref_required = None
-                if ref_doc.isSetPackageRequired(name):
-                    ref_required = ref_doc.getPackageRequired(name)
-                    # print('required: ', name, ref_required)
-
-                # FIXME: overwrite, assuming for not that the required is always identical for the packages
-                doc.setPackageRequired(name, ref_required)
-                print("set required: {} = {}".format(name, ref_required))
-
-            # add model definition for model
-            md = libsbml.ModelDefinition(ref_model)
-            comp_doc.addModelDefinition(md)
-
-        # remove the emds afterwards (do not remove while iterating over the list)
-        for emd_id in emd_ids:
-            # remove the emd from the model
-            comp_doc.removeExternalModelDefinition(emd_id)
-
-        # the replacement is done, but now we have to go through all ModelDefinitions
-        # and add package requirements in model definition :/
-        print("-"*80)
-        for k in range(doc.getNumPlugins()):
-            plugin = doc.getPlugin(k)
+            plugin = ref_doc.getPlugin(k)
+            uri = plugin.getURI()
+            prefix = plugin.getPrefix()
             name = plugin.getPackageName()
-            print(name, '\n')
-            md_list = comp_doc.getListOfModelDefinitions()
-            for md_model in md_list:
+            required = None
+            if ref_doc.isSetPackageRequired(name):
+                required = ref_doc.getPackageRequired(name)
 
-                # if a package needs something on
-                # a model we have to write it on all ModelDefinitions
-                # this will break on a package per package basis ! We know about fbc it needs
-                # a strict tag so writing this here
-                if name == "fbc":
-                    fbc_model = md_model.getPlugin(name)
-                    print('\tModel:', md_model)
-                    print('\tFBCModelPlugin:', fbc_model, type(fbc_model))
+            packages.append({
+                'model': ref_model,
+                'uri': uri,
+                'prefix': prefix,
+                'name': name,
+                'required': required
+            })
 
-                    # setting because it is required (if unlucky additional info required)
-                    # but we can't set it because we can't access the FBCModelPlugins of the ModelDefinitions
-                    if fbc_model is not None:
-                        if not fbc_model.isSetStrict():
-                            fbc_model.setStrict(False)
+    # remove emds from model (do not remove while iterating over the list)
+    for emd_id in emd_ids:
+        comp_doc.removeExternalModelDefinition(emd_id)
 
+    # activate all the packages
+    from pprint import pprint
+    pprint(packages)
+    for pdict in packages:
+        doc.enablePackage(pdict['uri'], pdict['prefix'], True)
+        doc.setPackageRequired(pdict['name'], pdict['required'])
+
+    # now add the model definitions
+    for md in mds:
+        comp_doc.addModelDefinition(md)
+
+    sbml_str = libsbml.writeSBMLToString(doc)
+    doc = libsbml.readSBMLFromString(sbml_str)
+
+    # replacement finished, now go through all ModelDefinitions
+    # and add things the packages require
+    print("-"*80)
+    for k in range(doc.getNumPlugins()):
+        plugin = doc.getPlugin(k)
+        name = plugin.getPackageName()
+        print(name, '\n')
+        md_list = comp_doc.getListOfModelDefinitions()
+        for md_model in md_list:
+
+            # if a package needs something on
+            # a model we have to write it on all ModelDefinitions
+            # this will break on a package per package basis ! We know about fbc it needs
+            # a strict tag so writing this here
+            if name == "fbc":
+                # for some ModelDefinitions we don't get the fbc Plugin ???
+                fbc_model = md_model.getPlugin(name)
+
+                print('\tModel:', md_model)
+                print('\tFBCModelPlugin:', fbc_model, type(fbc_model))
+
+                # setting because it is required (if unlucky additional info required)
+                # but we can't set it because we can't access the FBCModelPlugins of the ModelDefinitions
+                if fbc_model is not None:
+                    if not fbc_model.isSetStrict():
+                        fbc_model.setStrict(False)
+                else:
+                    print("WARNING: This should never happen. All ModelDefinitions should have a FBCModelPlugin")
 
     doc.checkInternalConsistency()
     doc.printErrors()
@@ -104,6 +120,7 @@ def flattenExternalModelDefinitions(doc):
 
 
 if __name__ == "__main__":
+    import libsbml
     from os.path import join as pjoin
 
     directory = './emd_files/'
@@ -111,8 +128,10 @@ if __name__ == "__main__":
 
     # replace the ExternalModelDefinitions with ModelDefinitions
     doc_top = libsbml.readSBMLFromFile(top_file)
-    doc_flat = flattenExternalModelDefinitions(doc_top)
-    from sbmlutils import sbmlio
-    sbmlio.write_sbml(doc_flat, pjoin(directory, 'test_emd_flat.xml'))
+    doc_no_emd = flattenExternalModelDefinitions(doc_top)
+
+    # write to file
+    libsbml.writeSBMLToFile(doc_no_emd, pjoin(directory, 'test_emd_flat.xml'))
+    print(libsbml.__version__)
 
 
