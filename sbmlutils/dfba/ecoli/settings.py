24cc973db8ce9e65be67f29a25128a8787996d20
Matthias Koenig <konigmatt@googlemail.com>
2017-11-11 20:39:24 +0100

    ecoli omex created

--- sbmlutils/dfba/ecoli/settings.py
+++ sbmlutils/dfba/ecoli/settings.py
@@ -1,14 +1,15 @@
 """
 Definition of constants.
 """
-
 from __future__ import absolute_import, print_function
 import os
 
+MODEL_ID = 'ecoli'
+VERSION = 10
+
 OUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
 
-MODEL_ID = 'ecoli'
-
+# SBML
 FBA_LOCATION = '{}_fba.xml'.format(MODEL_ID)
 BOUNDS_LOCATION = '{}_bounds.xml'.format(MODEL_ID)
 UPDATE_LOCATION = '{}_update.xml'.format(MODEL_ID)
@@ -19,4 +20,4 @@ FLATTENED_LOCATION = '{}_flattened.xml'.format(MODEL_ID)
 SEDML_LOCATION = 'dfba_simulation.xml'.format(MODEL_ID)
 
 # Combine archive
-OMEX_LOCATION = '{}-v{}.omex'.format(MODEL_ID, VERSION)
+OMEX_LOCATION = '{}_v{}.omex'.format(MODEL_ID, VERSION)
