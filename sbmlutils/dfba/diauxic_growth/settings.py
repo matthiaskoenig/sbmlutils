565a1eca1ee1f1855f0838f5a27402cae0d832ae
Matthias Koenig <konigmatt@googlemail.com>
2017-11-11 22:29:49 +0100

    latest model version diauxic created

--- sbmlutils/dfba/diauxic_growth/settings.py
+++ sbmlutils/dfba/diauxic_growth/settings.py
@@ -6,9 +6,9 @@ from __future__ import absolute_import, print_function
 import os
 
 
-OUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
-
 MODEL_ID = 'diauxic'
+VERSION = 13
+OUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'results')
 
 FBA_LOCATION = '{}_fba.xml'.format(MODEL_ID)
 BOUNDS_LOCATION = '{}_bounds.xml'.format(MODEL_ID)
@@ -16,15 +16,12 @@ UPDATE_LOCATION = '{}_update.xml'.format(MODEL_ID)
 TOP_LOCATION = '{}_top.xml'.format(MODEL_ID)
 FLATTENED_LOCATION = '{}_flattened.xml'.format(MODEL_ID)
 
-# annotations
-ANNOTATIONS_LOCATION = 'annotations.xlsx'
-
 # SED-ML
 SEDML_LOCATION = 'dfba_simulation.xml'.format(MODEL_ID)
 
 # Combine archive
-OMEX_LOCATION = '{}.omex'.format(MODEL_ID)
+OMEX_LOCATION = '{}_v{}.omex'.format(MODEL_ID, VERSION)
 
-# ExternalModelDefinitions removed
-top_noemd_file = '{}_top_noemd.xml'.format(MODEL_ID)
-flattened_noemd_file = '{}_flattened_noemd.xml'.format(MODEL_ID)
+# annotations
+ANNOTATIONS_LOCATION = 'annotations.xlsx'
+
