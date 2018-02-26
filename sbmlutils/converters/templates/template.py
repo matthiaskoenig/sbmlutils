ae02bff0c52b1bf18d57a3969d1fb39cd18200c1
Matthias Koenig <konigmatt@googlemail.com>
2018-02-26 15:14:50 +0100

    working scipy integration

--- sbmlutils/converters/templates/template.py
+++ sbmlutils/converters/templates/template.py
@@ -45,3 +45,42 @@ def f_dxdt(x, t, p):
         {{ dx[id] }},       # [{{ loop.index0 }}] {{ id }}
         {% endfor %}
     ]
+
+
+def f_y(x, t, p):
+    """ Calculate y.
+    :param x:
+    :param t:
+    :param p:
+    :return:
+    """
+
+    {% for id in yids %}
+    {{ id }} = {{y[id]}}  # [{{ loop.index0 }}] {{ id }}
+    {% endfor %}
+
+    # --------------------------------------
+
+    y = np.empty(shape=({{yids | length}}))
+    {% for id in yids %}
+    y[{{loop.index0}}] = {{ id }}  # [{{ loop.index0 }}] {{ id }}
+    {% endfor %}
+
+    return y
+
+
+def f_z(X, T, p):
+    """ DataFrame of full timecourse of solution. """
+    (Nt, Nx) = X.shape
+    Ny = len(yids)
+    Nz = 1 + Nx + Ny
+    columns = ["time"] + xids + yids
+    Z = np.empty(shape=(Nt, Nz))
+    Z[:,0] = T
+    Z[:,1:(Nx+1)] = X
+    for kt in range(Nt):
+        y = f_y(x=X[kt, :], t=T[kt], p=p)
+        Z[kt, (Nx+1):] = y
+
+    Z = pd.DataFrame(Z, columns = columns)
+    return Z
