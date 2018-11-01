caae61c8acedca6fe101ac7e01ba094e04ac97a4
Matthias Koenig <konigmatt@googlemail.com>
2018-11-01 12:58:17 +0100

    fixing path issues in dfba examples

--- wheel_install.sh
+++ wheel_install.sh
@@ -2,6 +2,7 @@
 # install the updated dependencies
 PY=36
 URL=https://github.com/sys-bio/wheelhouse/blob/master
+mkdir wheels
 wget $URL/tesedml-0.4.3.1-cp36-cp36m-manylinux1_x86_64.whl?raw=true -O wheels/tesedml-0.4.3.1-cp36-cp36m-manylinux1_x86_64.whl
 wget $URL/tesbml-5.16.0.0-cp36-cp36m-manylinux1_x86_64.whl?raw=true -O wheels/tesbml-5.16.0.0-cp36-cp36m-manylinux1_x86_64.whl
 wget $URL/tecombine-0.2.2.1-cp36-cp36m-manylinux1_x86_64.whl?raw=true -O wheels/tecombine-0.2.2.1-cp36-cp36m-manylinux1_x86_64.whl
