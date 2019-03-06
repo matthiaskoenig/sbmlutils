267c6378a10523701662f4cfa7ed600418fbda4f
Matthias Koenig <konigmatt@googlemail.com>
2019-03-06 15:51:05 +0100

    working on boundaryCondition

--- sbmlutils/examples/models/fbc/factory.py
+++ sbmlutils/examples/models/fbc/factory.py
@@ -1,6 +1,5 @@
 import os
 from sbmlutils.modelcreator.creator import Factory
-from sbmlutils.examples.models.fbc import fbc2
 
 
 def create(tmp=False):
@@ -10,7 +9,11 @@ def create(tmp=False):
     """
     models_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
-    factory = Factory(modules=['sbmlutils.examples.models.fbc.fbc2'],
+    factory = Factory(modules=['sbmlutils.examples.models.fbc.fbc_ex1'],
+                      target_dir=os.path.join(models_dir, 'results'))
+    factory.create(tmp)
+
+    factory = Factory(modules=['sbmlutils.examples.models.fbc.fbc_ex2'],
                       target_dir=os.path.join(models_dir, 'results'))
     factory.create(tmp)
 
