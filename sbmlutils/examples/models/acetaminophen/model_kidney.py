a9b78f25d85989a554dcbf0b1d127e6683bdb2e3
Janosch Brandhorst <brandhorst@landau.itb.pri>
2019-12-13 16:01:41 +0100

    paracetamolmodel updated

--- sbmlutils/examples/models/acetaminophen/model_kidney.py
+++ sbmlutils/examples/models/acetaminophen/model_kidney.py
@@ -79,8 +79,8 @@ reactions = [
              equation="apap_ext <-> apap",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAP_Import_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAP_Import_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAP_Import_Vmax", 0.005, unit=UNIT_mmole_per_min),
+                 Parameter("APAP_Import_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAP_Import_Vmax* (apap_ext/Vext)/(apap_ext/Vext+APAP_Import_Km) ", UNIT_mmole_per_min)),
 
@@ -88,8 +88,8 @@ reactions = [
              equation="apapglu_ext <-> apapglu",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPGLU_Import_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPGLU_Import_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPGLU_Import_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPGLU_Import_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPGLU_Import_Vmax* (apapglu_ext/Vext)/(apapglu_ext/Vext+APAPGLU_Import_Km) ", UNIT_mmole_per_min)),
 
@@ -97,8 +97,8 @@ reactions = [
              equation="apapsul_ext <-> apapsul",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPSUL_Import_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPSUL_Import_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPSUL_Import_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPSUL_Import_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPSUL_Import_Vmax* (apapsul_ext/Vext)/(apapsul_ext/Vext+APAPSUL_Import_Km) ", UNIT_mmole_per_min)),
 
@@ -106,24 +106,24 @@ reactions = [
              equation="apapcys_ext <-> apapcys",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPCYS_Import_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPCYS_Import_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPCYS_Import_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPCYS_Import_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPCYS_Import_Vmax* (apapcys_ext/Vext)/(apapcys_ext/Vext+APAPCYS_Import_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPGSHIM",
              equation="apapgsh_ext <-> apapgsh",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPGSH_Import_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPGSH_Import_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPGSH_Import_Vmax",0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPGSH_Import_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPGSH_Import_Vmax* (apapgsh_ext/Vext)/(apapgsh_ext/Vext+APAPGSH_Import_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPMERIM",
              equation="apapmer_ext <-> apapmer",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPMER_Import_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPMER_Import_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPMER_Import_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPMER_Import_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPMER_Import_Vmax* (apapmer_ext/Vext)/(apapmer_ext/Vext+APAPMER_Import_Km) ", UNIT_mmole_per_min)),
 
@@ -133,40 +133,40 @@ reactions = [
              equation="apap <-> apapglu",
              sboTerm=SBO_BIOCHEMICAL_REACTION,
              pars=[
-                 Parameter("APAPGLU_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPGLU_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPGLU_Vmax", 1e-7, unit=UNIT_mmole_per_min),
+                 Parameter("APAPGLU_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPGLU_Vmax * (apap/Vki)/(apap/Vki+APAPGLU_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPSUL",
              equation="apap -> apapsul",
              sboTerm=SBO_BIOCHEMICAL_REACTION,
              pars=[
-                 Parameter("APAPSUL_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPSUL_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPSUL_Vmax", 1e-7, unit=UNIT_mmole_per_min),
+                 Parameter("APAPSUL_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPSUL_Vmax * (apap/Vki)/(apap/Vki+APAPSUL_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPCYS",
              equation="apap -> apapcys",
              sboTerm=SBO_BIOCHEMICAL_REACTION,
              pars=[
-                 Parameter("APAPCYS_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPCYS_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPCYS_Vmax", 1e-7, unit=UNIT_mmole_per_min),
+                 Parameter("APAPCYS_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPCYS_Vmax * (apap/Vki)/(apap/Vki+APAPCYS_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPGSH",
              equation="apap -> apapgsh",
              sboTerm=SBO_BIOCHEMICAL_REACTION,
              pars=[
-                 Parameter("APAPGSH_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPGSH_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPGSH_Vmax", 1e-7, unit=UNIT_mmole_per_min),
+                 Parameter("APAPGSH_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPGSH_Vmax * (apap/Vki)/(apap/Vki+APAPGSH_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPMER",
              equation="apap -> apapmer",
              sboTerm=SBO_BIOCHEMICAL_REACTION,
              pars=[
-                 Parameter("APAPMER_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPMER_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPMER_Vmax", 1e-7, unit=UNIT_mmole_per_min),
+                 Parameter("APAPMER_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPMER_Vmax * (apap/Vki)/(apap/Vki+APAPMER_Km) ", UNIT_mmole_per_min)),
 
@@ -176,8 +176,8 @@ reactions = [
              equation="apap <-> apap_urine",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAP_Export_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAP_Export_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAP_Export_Vmax", 0.0005, unit=UNIT_mmole_per_min),
+                 Parameter("APAP_Export_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAP_Export_Vmax* (apap/Vki)/(apap/Vki+APAP_Export_Km) ", UNIT_mmole_per_min)),
 
@@ -185,8 +185,8 @@ reactions = [
              equation="apapglu <-> apapglu_urine",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPGLU_Export_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPGLU_Export_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPGLU_Export_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPGLU_Export_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPGLU_Export_Vmax* (apapglu/Vki)/(apapglu/Vki+APAPGLU_Export_Km) ", UNIT_mmole_per_min)),
 
@@ -194,8 +194,8 @@ reactions = [
              equation="apapsul <-> apapsul_urine",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPSUL_Export_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPSUL_Export_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPSUL_Export_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPSUL_Export_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPSUL_Export_Vmax* (apapsul/Vki)/(apapsul/Vki+APAPSUL_Export_Km) ", UNIT_mmole_per_min)),
 
@@ -203,24 +203,24 @@ reactions = [
              equation="apapcys <-> apapcys_urine",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPCYS_Export_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPCYS_Export_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPCYS_Export_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPCYS_Export_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPCYS_Export_Vmax* (apapcys/Vki)/(apapcys/Vki+APAPCYS_Export_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPGSHEXP",
              equation="apapgsh <-> apapgsh_urine",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPGSH_Export_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPGSH_Export_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPGSH_Export_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPGSH_Export_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPGSH_Export_Vmax* (apapgsh/Vki)/(apapgsh/Vki+APAPGSH_Export_Km) ", UNIT_mmole_per_min)),
     Reaction("APAPMEREXP",
              equation="apapmer <-> apapmer_urine",
              sboTerm=SBO_TRANSPORT_REACTION,
              pars=[
-                 Parameter("APAPMER_Export_Vmax", 1.0, unit=UNIT_mmole_per_min),
-                 Parameter("APAPMER_Export_Km", 1.0, unit=UNIT_mM),
+                 Parameter("APAPMER_Export_Vmax", 0.05, unit=UNIT_mmole_per_min),
+                 Parameter("APAPMER_Export_Km", 0.05, unit=UNIT_mM),
              ],
              formula=("APAPMER_Export_Vmax* (apapmer/Vki)/(apapmer/Vki+APAPMER_Export_Km) ", UNIT_mmole_per_min)),
 ]
