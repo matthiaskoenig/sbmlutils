2d6ac10c4f313cca9e9536359cba69493b6137e2
Matthias Koenig <konigmatt@googlemail.com>
2020-09-29 16:53:52 +0200

    fixes for sbmlutils

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-"""Set up the sbmlutils package."""
 from setuptools import setup
 
-
-# All other arguments are defined in `setup.cfg`.
 if __name__ == "__main__":
-    setup(version="0.4.0")
+    setup(version="0.4.1a1")
--- src/sbmlutils/__init__.py
+++ src/sbmlutils/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 __author__ = "Matthias Koenig"
-__version__ = "0.4.0"
+__version__ = "0.4.1a1"
 
 program_name = "sbmlutils"
 
