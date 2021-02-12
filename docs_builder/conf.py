4b40135ee83d13ac01dd48af7538964b9c4de30d
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-02-12 18:09:39 +0530

    doc: generated API documentation using sphinx autoapi

--- docs_builder/conf.py
+++ docs_builder/conf.py
@@ -12,15 +12,17 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys
-import sphinx_rtd_theme
 import os
+import sys
 
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
+import sphinx_rtd_theme
 
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('.'))
+on_rtd = os.environ.get("READTHEDOCS") == "True"
+
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("."))
 
 # In order to build documentation that requires libraries to import
 class Mock(object):
@@ -32,13 +34,13 @@ class Mock(object):
 
     @classmethod
     def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
+        if name in ("__file__", "__path__"):
+            return "/dev/null"
         else:
             return Mock()
 
 
-MOCK_MODULES = ['libsbml', 'cobrapy', 'roadrunner']
+MOCK_MODULES = ["libsbml", "cobrapy", "roadrunner"]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
@@ -54,37 +56,45 @@ MOCK_MODULES = []
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'nbsphinx',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.autosectionlabel',
+    "nbsphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosectionlabel",
+    "autoapi.extension",
 ]
+
+# source directory for generation of API documentation
+autoapi_dirs = ["../src"]
+
+# exclusion of template files from build process
+autoapi_ignore = ["*template*"]
+
 # execution of notebooks
 if on_rtd:
-    nbsphinx_execute = 'auto'
+    nbsphinx_execute = "auto"
 else:
-    nbsphinx_execute = 'always'
+    nbsphinx_execute = "always"
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'sbmlutils'
-copyright = u'2017-2020, Matthias König'
-author = u'Matthias König'
+project = u"sbmlutils"
+copyright = u"2017-2020, Matthias König"
+author = u"Matthias König"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -92,7 +102,9 @@ author = u'Matthias König'
 #
 
 from sbmlutils import __version__ as release
-version = '.'.join(release.split('.')[:3])
+
+
+version = ".".join(release.split(".")[:3])
 # The short X.Y version.
 # version = u'0.1.1'
 # The full version, including alpha/beta/rc tags.
@@ -113,7 +125,7 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -131,7 +143,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
@@ -176,7 +188,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -239,20 +251,17 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'sbmlutilsdoc'
+htmlhelp_basename = "sbmlutilsdoc"
 
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     # 'preamble': '',
-
     # Latex figure (float) alignment
     # 'figure_align': 'htbp',
 }
@@ -261,8 +270,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sbmlutils.tex', u'sbmlutils Documentation',
-     u'Matthias König', 'manual'),
+    (
+        master_doc,
+        "sbmlutils.tex",
+        u"sbmlutils Documentation",
+        u"Matthias König",
+        "manual",
+    ),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -290,10 +304,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'sbmlutils', u'sbmlutils Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "sbmlutils", u"sbmlutils Documentation", [author], 1)]
 
 # If true, show URL addresses after external links.
 # man_show_urls = False
@@ -305,9 +316,15 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sbmlutils', u'sbmlutils Documentation',
-     author, 'sbmlutils', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "sbmlutils",
+        u"sbmlutils Documentation",
+        author,
+        "sbmlutils",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 # Documents to append as an appendix to all manuals.
