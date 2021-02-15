4b1efd8f23c5116ebc8b9f99e80b15a5fe0b83f9
Matthias Koenig <konigmatt@googlemail.com>
2021-02-15 20:28:27 +0100

    improved testing, removed duplication, ids for parametrization

--- src/sbmlutils/test/report/test_report.py
+++ src/sbmlutils/test/report/test_report.py
@@ -1,6 +1,6 @@
-"""
-Test SBML report.
-"""
+"""Test SBML report."""
+from pathlib import Path
+
 import pytest
 
 from sbmlutils.report import sbmlreport
@@ -10,17 +10,78 @@ from sbmlutils.test import (
     GALACTOSE_SINGLECELL_SBML,
     GLUCOSE_SBML,
     GZ_SBML,
+    REPRESSILATOR_SBML,
     VDP_SBML,
 )
 
+sbml_paths = [
+    BASIC_SBML,
+    DEMO_SBML,
+    GALACTOSE_SINGLECELL_SBML,
+    GLUCOSE_SBML,
+    GZ_SBML,
+    REPRESSILATOR_SBML,
+    VDP_SBML,
+]
 
-@pytest.mark.parametrize(
-    "source", [DEMO_SBML, GALACTOSE_SINGLECELL_SBML, GLUCOSE_SBML, BASIC_SBML, VDP_SBML]
-)
-def test_report(source, tmp_path):
-    sbmlreport.create_report(sbml_path=source, output_dir=tmp_path)
+
+def sbml_paths_idfn(sbml_path):
+    return(sbml_path.name)
 
 
-@pytest.mark.parametrize("source", [GZ_SBML])
-def test_report_gz(source, tmp_path):
-    sbmlreport.create_report(sbml_path=source, output_dir=tmp_path)
+@pytest.mark.parametrize("sbml_path", sbml_paths, ids=sbml_paths_idfn)
+def test_report(sbml_path, tmp_path):
+    """Test report generation."""
+    sbmlreport.create_report(sbml_path=sbml_path, output_dir=tmp_path)
+
+
+@pytest.mark.parametrize("sbml_path", [GZ_SBML], ids=sbml_paths_idfn)
+def test_report_gz(sbml_path, tmp_path):
+    """Test report generation for GZ reports."""
+    sbmlreport.create_report(sbml_path=sbml_path, output_dir=tmp_path)
+
+
+@pytest.mark.parametrize("sbml_path", sbml_paths, ids=sbml_paths_idfn)
+def test_report_latex(sbml_path: Path, tmp_path):
+    """Test creation of report with Latex Math."""
+    check_report_math_type(
+        sbml_path=sbml_path,
+        math_type="latex",
+        tmp_path=tmp_path
+    )
+
+
+@pytest.mark.parametrize("sbml_path", sbml_paths, ids=sbml_paths_idfn)
+def test_report_cmathml(sbml_path: Path, tmp_path):
+    """Test creation of report with Content MathML."""
+    check_report_math_type(
+        sbml_path=sbml_path,
+        math_type="cmathml",
+        tmp_path=tmp_path
+    )
+
+
+@pytest.mark.parametrize("sbml_path", sbml_paths, ids=sbml_paths_idfn)
+def test_report_pmathml(sbml_path: Path, tmp_path):
+    """Test creation of report with Presentation MathML."""
+    check_report_math_type(
+        sbml_path=sbml_path,
+        math_type="cmathml",
+        tmp_path=tmp_path
+    )
+
+
+def check_report_math_type(sbml_path: Path, math_type: str, tmp_path):
+    """Checks SBML report with given math type."""
+    sbmlreport.create_report(
+        sbml_path=sbml_path,
+        output_dir=tmp_path,
+        math_type=math_type
+    )
+
+    basename = sbml_path.name
+    name = ".".join(basename.split(".")[:-1])
+
+    # check HTML report created
+    path_html = tmp_path / f"{name}.html"
+    assert path_html.exists()
