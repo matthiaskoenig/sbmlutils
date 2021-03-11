6ac4f774bab14d7899ac652e180a73f7042b1658
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-03-11 23:24:10 +0530

    Revert "added functionality to render initial JSON data"
    This reverts commit 39d00d8c6364c5aad94764d2edcdf24aca6359c4.

--- vue-report/src/App.vue
+++ vue-report/src/App.vue
@@ -1,36 +1,30 @@
 <template>
   <div id="app">
-    <page-header></page-header>
-    <app-report ref="report"></app-report>
+    <Header />
+    <Report />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import FileUpload from './components/FileUpload.vue'
 import Report from './components/Report.vue'
 
 export default {
   name: 'app',
   components: {
-    'page-header': Header,
-    'app-report': Report
+    Header,
+    FileUpload,
+    Report
   },
   data () {
     return {
-      msg: 'SBML Report Rendering Prototype',
-      initial_json: {}
+      msg: 'SBML Report Rendering Prototype'
     }
-  },
-  created() {
-    var json = JSON.parse(document.getElementById("initial").textContent)
-    this.initial_json = json
-  },
-  mounted: function(){
-    this.$refs.report.updateJSON(this.initial_json)
   },
   methods: {
 
-  },
+  }
 }
 </script>
 
