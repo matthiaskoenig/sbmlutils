24e2d0648f07061660c4aa6e000ccc2e64fb26cd
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-05-29 21:23:44 +0530

    refactored the vue app, added bs4

--- sbml4humans/src/App.vue
+++ sbml4humans/src/App.vue
@@ -1,30 +1,21 @@
 <template>
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-    <router-view />
+    <navbar />
+    <main-window />
 </template>
 
-<style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
+<script lang="ts">
+import Navbar from '@/components/Navbar.vue';
+import MainWindow from '@/components/MainWindow.vue';
 
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+export default {
+    name: 'app',
+    components: {
+        'navbar': Navbar,
+        'main-window': MainWindow,
     }
 }
+</script>
+
+<style lang="scss">
+@import '../src/assets/styles/scss/App.scss';
 </style>
--- sbml4humans/src/components/HomeInfo.vue
+++ sbml4humans/src/components/HomeInfo.vue
@@ -25,12 +25,11 @@ import { Options, Vue } from "vue-class-component";
         msg: String,
     },
 })
-export default class HelloWorld extends Vue {
+export default class HomeInfo extends Vue {
     msg!: string;
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    @import '../assets/styles/scss/components/HelloWorld.scss';
+@import '../assets/styles/scss/components/HomeInfo.scss';
 </style>
