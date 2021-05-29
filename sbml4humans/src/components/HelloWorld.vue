64cb05ae4c9a6679bf2aacfbff80727be067b672
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-05-29 19:09:57 +0530

    initiated vue project

---
+++ sbml4humans/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+
+        <h3>Essential Links</h3>
+        <ul>
+            <li>
+                <a href="https://https://github.com/matthiaskoenig/sbmlutils" target="_blank" rel="noopener">sbmlutils</a>
+            </li>
+            <li>
+                <a href="http://sbml.org" target="_blank" rel="noopener">SBML</a>
+            </li>
+            <li>
+                <a href="https://summerofcode.withgoogle.com/projects/#5355377035575296" target="_blank" rel="noopener">Project Description</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    props: {
+        msg: String,
+    },
+})
+export default class HelloWorld extends Vue {
+    msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    @import '../assets/styles/scss/components/HelloWorld.scss';
+</style>
---
+++ sbml4humans/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/images/logo.png" />
+        <HelloWorld msg="Generate SBML Model Reports on the go!" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+    components: {
+        HelloWorld,
+    },
+})
+export default class Home extends Vue {}
+</script>
