9639c98fb5ef9b9c50f23165bcdb7a5c8bf0b307
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-06-06 02:03:11 +0530

    added SBaseInfo to detail container

--- sbml4humans/src/components/DetailContainer.vue
+++ sbml4humans/src/components/DetailContainer.vue
@@ -1,31 +1,60 @@
 <template>
     <div class="detail-container">
         <!-- SBASE INFO -->
-        <a-descriptions
-            bordered
+        <div class="data" v-if="info.name">
+            <div class="label"><strong>Name:</strong> {{ info.name }}</div>
+        </div>
+        <div class="data" v-if="info.sbmlType">
+            <div class="label"><strong>SBML Type:</strong> {{ info.sbmlType }}</div>
+        </div>
+        <div class="data" v-if="info.id">
+            <div class="label"><strong>SID:</strong> {{ info.id }}</div>
+        </div>
+        <div class="data" v-if="info.sbo">
+            <div class="label"><strong>SBO:</strong> {{ info.sbo }}</div>
+        </div>
+        <div class="data" v-if="info.metaId">
+            <div class="label"><strong>Meta ID:</strong> {{ info.metaId }}</div>
+        </div>
+        <div class="data" v-if="info.history">
+            <div class="label"><strong>History:</strong></div>
+            <br />
+            <div class="ml-4">
+                <div class="label">Date Created: {{ info.history.createdDate }}</div>
+                <br />
+                <div class="label">Creators:</div>
+                <ul title="Creators">
+                    <li
+                        v-for="creator in info.history.creators"
+                        v-bind:key="creator.email"
+                    >
+                        {{ creator.givenName }} {{ creator.familyName }},
+                        {{ creator.organization }} (<a
+                            v-bind:href="`mailto:${creator.email}`"
+                            >{{ creator.email }}</a
+                        >)
+                    </li>
+                </ul>
+                <div class="label">Dates Modified:</div>
+                <ul title="Dates Modified">
+                    <li v-for="date in info.history.modifiedDates" v-bind:key="date">
+                        {{ date }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data" v-if="info.xml">
+            <a-button type="info" @click="showModal">View XML</a-button>
+        </div>
+        <a-modal
+            v-model:visible="visible"
+            width="1000px"
             v-bind:title="`${info.name} - ${info.sbo}`"
-            :size="size"
+            v-bind:footer="null"
+            @ok="handleOk"
         >
-            <a-descriptions-item label="Name">{{ info.name }}</a-descriptions-item>
-            <a-descriptions-item label="SID">{{ info.id }}</a-descriptions-item>
-            <a-descriptions-item label="SBO">{{ info.sbo }}</a-descriptions-item>
-            <a-descriptions-item label="Meta ID">{{ info.metaId }}</a-descriptions-item>
-            <a-descriptions-item label="SBML Type">{{
-                info.sbmlType
-            }}</a-descriptions-item>
-            <a-descriptions-item label="XML">
-                <a-button type="info" @click="showModal">View Source</a-button>
-                <a-modal
-                    v-model:visible="visible"
-                    width="1000px"
-                    v-bind:title="`${info.name} - ${info.sbo}`"
-                    v-bind:footer="null"
-                    @ok="handleOk"
-                >
-                    <p>{{ info.xml }}</p>
-                </a-modal>
-            </a-descriptions-item>
-        </a-descriptions>
+            <p>{{ info.xml }}</p>
+        </a-modal>
 
         <br />
 
