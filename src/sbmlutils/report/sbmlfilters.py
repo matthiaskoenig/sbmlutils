20afbeb8955c445e5c4faaf9a10d60b6e4a306ec
Matthias Koenig <konigmatt@googlemail.com>
2021-06-18 15:14:00 +0200

    starting backend refactoring

--- src/sbmlutils/report/sbmlfilters.py
+++
@@ -1,9 +0,0 @@
-"""Template filters for rendering SBML.
-
-Additional functionality for templates like displaying
-annotations or rendering of units.
-"""
-from typing import List
-
-
-filters: List = []
