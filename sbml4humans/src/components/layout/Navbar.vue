9a7a589587d56d4d116313a0f605a495cd49b4f4
Matthias Koenig <konigmatt@googlemail.com>
2021-06-25 12:52:15 +0200

    code review

--- sbml4humans/src/components/layout/Navbar.vue
+++ sbml4humans/src/components/layout/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <router-link class="navbar-brand" to="/">
             <img
                 class="logo"
