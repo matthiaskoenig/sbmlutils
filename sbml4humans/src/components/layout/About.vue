678d69b05928eab4906bf2eda3cc4d3dca02ac02
Matthias Koenig <konigmatt@googlemail.com>
2021-08-23 11:44:10 +0200

    Fixing minor issue with about page (#268)

--- sbml4humans/src/components/layout/About.vue
+++ sbml4humans/src/components/layout/About.vue
@@ -7,13 +7,15 @@
             <h2>Systems Biology Markup Language</h2>
 
             <div class="p-d-flex content">
-                <img src="@/assets/images/sbml-logo.png" class="image" width="100" />
+                <a href="http://sbml.org" target="_blank">
+                    <img src="@/assets/images/sbml-logo.png" class="image" width="100"/>
+                </a>
                 <p>
                     The Systems Biology Markup Language (<a
-                        href="http://sbml.org"
-                        target="_blank"
-                        >SBML</a
-                    >) is a standard format for the representation and exchange of
+                    href="http://sbml.org"
+                    target="_blank"
+                >SBML</a
+                >) is a standard format for the representation and exchange of
                     models. The information in an SBML model file is organized as a list
                     of components encapsulated within the model (e.g. compartments,
                     species, parameters, reactions).
@@ -32,9 +34,9 @@
                 is to provide an interactive and reactive report for SBML models which
                 which will allow humans (experts as well as beginners) to easily
                 comprehend the content of a model. SBML4Humans is part of the
-                <a target="_blank" href="https://github.com/matthiaskoenig/sbmlutils"
-                    >sbmlutils</a
-                >
+                <a target="_blank" href="https://github.com/matthiaskoenig/sbmlutils">
+                    sbmlutils
+                </a>
                 project.
             </div>
         </div>
@@ -46,14 +48,12 @@
                 <ul title="Developers">
                     <li>
                         <a href="https://livermetabolism.com" target="_blank"
-                            >Dr. Matthias König</a
-                        >, Humboldt University, Berlin
+                        >Dr. Matthias König</a>, Humboldt University, Berlin
                     </li>
                     <li>Dr. Ralf Steuer, Humboldt University, Berlin</li>
                     <li>
                         <a href="https://sankha555.github.io/index" target="_blank"
-                            >Sankha Das</a
-                        >, BITS Pilani, Rajasthan, India
+                        >Sankha Das</a>, BITS Pilani, Rajasthan, India
                     </li>
                 </ul>
             </div>
@@ -63,10 +63,12 @@
         <div class="p-mb-2">
             <h2>Funding</h2>
             <div class="p-d-flex content">
-                <img src="@/assets/images/gsoc.jpeg" class="image" width="100" />
+                <a href="https://summerofcode.withgoogle.com/" target="_blank">
+                    <img src="@/assets/images/gsoc.jpeg" class="image" width="100" />
+                </a>
                 <p>
-                    <a href="https://summerofcode.withgoogle.com/"
-                        >Google Summer of Code 2021</a
+                    <a href="https://summerofcode.withgoogle.com/" target="_blank"
+                    >Google Summer of Code 2021</a
                     >
                     A global program focused on bringing more student developers into
                     open source software development. Students work with an open source
@@ -79,12 +81,12 @@
                     Matthias König is supported by the Federal Ministry of Education and
                     Research (BMBF, Germany) within the research network Systems
                     Medicine of the Liver (<a href="https://lisym.org/" target="_blank"
-                        >LiSyM</a
-                    >, grant number 031L0054) and by the German Research Foundation
+                >LiSyM</a
+                >, grant number 031L0054) and by the German Research Foundation
                     (DFG) within the Research Unit Programme FOR 5151 "<a
-                        href="https://qualiperf.de"
-                        >QuaLiPerF</a
-                    >
+                    href="https://qualiperf.de"
+                >QuaLiPerF</a
+                >
                     (Quantifying Liver Perfusion-Function Relationship in Complex
                     Resection - A Systems Medicine Approach)" by grant number 436883643.
                 </p>
@@ -101,17 +103,17 @@
                             href="https://zenodo.org/badge/latestdoi/55952847"
                             target="_blank"
                             rel="nofollow"
-                            ><img
-                                alt="Zenodo DOI"
-                                src="https://camo.githubusercontent.com/332b1f7dd91e445f074084a39ed733363fa353b257d9e526d489c88dab025172/68747470733a2f2f7a656e6f646f2e6f72672f62616467652f35353935323834372e737667"
-                                data-canonical-src="https://zenodo.org/badge/55952847.svg"
-                                style="max-width: 100%"
+                        ><img
+                            alt="Zenodo DOI"
+                            src="https://camo.githubusercontent.com/332b1f7dd91e445f074084a39ed733363fa353b257d9e526d489c88dab025172/68747470733a2f2f7a656e6f646f2e6f72672f62616467652f35353935323834372e737667"
+                            data-canonical-src="https://zenodo.org/badge/55952847.svg"
+                            style="max-width: 100%"
                         /></a>
                     </li>
                     <li>
                         Project Repository:
                         <a href="https://github.com/matthiaskoenig/sbmlutils"
-                            >sbmlutils</a
+                        >sbmlutils</a
                         >
                     </li>
                 </ul>
@@ -121,7 +123,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
 /**
  * Container to show About info about the project
@@ -140,6 +142,7 @@ export default defineComponent({
     max-width: 100%;
     margin: auto 30px;
 }
+
 .content {
     text-align: justify;
 }
