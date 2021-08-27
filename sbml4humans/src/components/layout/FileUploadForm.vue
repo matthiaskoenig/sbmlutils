1c00e4628e4cbf265eca2e00a71b51898cd23c8f
sankha555 <f20190029@pilani.bits-pilani.ac.in>
2021-08-27 19:59:50 +0530

    fixes #292 : privacy policy link fixed

--- sbml4humans/src/components/layout/FileUploadForm.vue
+++ sbml4humans/src/components/layout/FileUploadForm.vue
@@ -19,7 +19,7 @@
                 </p>
             </template>
         </FileUpload>
-        <p class="p-pt-3" style="font-size: smaller">By using any part of this service, you agree to the terms of the <a href="https://github.com/matthiaskoenig/sbmlutils/blob/sbml4humans/develop/privacy_notice.md" target="_blank">privacy notice</a>.</p>
+        <p class="p-pt-3" style="font-size: smaller">By using any part of this service, you agree to the terms of the <a href="https://github.com/matthiaskoenig/sbmlutils/blob/develop/sbml4humans/privacy_notice.md" target="_blank">privacy notice</a>.</p>
         <loading parent="file" />
 
     </div>
