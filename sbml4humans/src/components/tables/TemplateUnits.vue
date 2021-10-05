085460caeea5c617750cffbc012b2c45b49568eb
Matthias Koenig <konigmatt@googlemail.com>
2021-10-05 23:09:05 +0200

    import fix

--- sbml4humans/src/components/tables/TemplateUnits.vue
+++ sbml4humans/src/components/tables/TemplateUnits.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
-import Katex from "@/components/layout/Katex";
+import Katex from "@/components/layout/Katex.vue";
 export default defineComponent({
     components: {
         Katex,
