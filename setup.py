090ea3240abc951ea1492b72d8fdb15a31c3de9c
Matthias Koenig <konigmatt@googlemail.com>
2021-10-10 16:58:42 +0200

    Bump version: 0.5.7 → 0.5.8

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from setuptools import setup  # type: ignore
 
 
 if __name__ == "__main__":
-    setup(version="0.5.7")
+    setup(version="0.5.8")
--- src/sbmlutils/__init__.py
+++ src/sbmlutils/__init__.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 __author__ = "Matthias Koenig"
-__version__ = "0.5.7"
+__version__ = "0.5.8"
 
 
 from sbmlutils.utils import show_versions
