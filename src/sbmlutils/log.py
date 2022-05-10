10137e5c4c30d3a19f4ff84f01343c8e8ff906e0
Matthias Koenig <konigmatt@googlemail.com>
2022-05-10 13:36:06 +0200

    logging helpers

--- src/sbmlutils/log.py
+++ src/sbmlutils/log.py
@@ -26,3 +26,16 @@ def get_logger(name: str, level: int = logging.INFO) -> logging.Logger:
     logger.setLevel(level)
     logger.addHandler(handler)
     return logger
+
+
+def set_level_for_all_loggers(level: int = logging.DEBUG):
+    """Set log level for all loggers.
+
+    This is mainly used in debugging to get additional information.
+    FIXME: add resetting functionality
+    """
+    loggers: List[Logger] = [
+        logging.getLogger(name) for name in logging.root.manager.loggerDict
+    ]
+    for logger in loggers:
+        logger.setLevel(level)
