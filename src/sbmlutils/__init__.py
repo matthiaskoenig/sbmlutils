a33eaaf11136e6f26104f8fe7a2afc6849f21d50
Matthias Koenig <konigmatt@googlemail.com>
2022-05-31 16:51:49 +0200

    updated dependencies

--- src/sbmlutils/__init__.py
+++ src/sbmlutils/__init__.py
@@ -5,9 +5,6 @@ __author__ = "Matthias Koenig"
 __version__ = "0.7.5"
 
 
-from sbmlutils.utils import show_versions
-
-
 program_name = "sbmlutils"
 
 RESOURCES_DIR = Path(__file__).parent / "resources"
--- tests/test_utils.py
+++
@@ -1,7 +0,0 @@
-"""Test show versions."""
-from sbmlutils.utils import show_versions
-
-
-def test_show_versions() -> None:
-    """Test show versions."""
-    show_versions()
