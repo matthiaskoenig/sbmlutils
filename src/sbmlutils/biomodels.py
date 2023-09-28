e6f094b3d1ae212cfba4ba95b8cef339e250cd48
Matthias König <konigmatt@googlemail.com>
2023-09-28 13:02:19 +0200

    changes for release

--- src/sbmlutils/biomodels.py
+++ src/sbmlutils/biomodels.py
@@ -21,15 +21,12 @@ def download_file(url: str, path: Path) -> Path:
 
     Raises :class:`HTTPError`, if one occurred.
     """
-
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
         with open(path, "wb") as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                # if chunk:
+            for chunk in r.iter_content(chunk_size=1024):
                 f.write(chunk)
+
     return path
 
 
