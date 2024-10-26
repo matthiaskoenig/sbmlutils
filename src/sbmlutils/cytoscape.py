b6eed1fd2fcdf9c658e91a4a6156749f9282b47e
Matthias König <konigmatt@googlemail.com>
2024-10-26 19:21:01 +0200

    working on visualization

--- src/sbmlutils/cytoscape.py
+++ src/sbmlutils/cytoscape.py
@@ -3,12 +3,14 @@
 import os
 import tempfile
 
+import pandas as pd
 
 os.environ["PY4CYTOSCAPE_DETAIL_LOGGER_DIR"] = str(tempfile.gettempdir())
 
 from pathlib import Path  # noqa: E402
-from typing import Any, Union  # noqa: E402
+from typing import Any, Union, Optional  # noqa: E402
 
+import libsbml
 import py4cytoscape as p4c  # type: ignore  # noqa: E402
 from requests.exceptions import RequestException  # noqa: E402
 
@@ -30,8 +32,13 @@ def visualize_antimony(source: Union[Path, str], delete_session: bool = False) -
     visualize_sbml(Path(f_tmp.name), delete_session=delete_session)
 
 
-def visualize_sbml(sbml_path: Path, delete_session: bool = False) -> None:
-    """Visualize SBML networks in cytoscape."""
+def visualize_sbml(sbml_path: Path, delete_session: bool = False) -> Optional[int]:
+    """Visualize SBML networks in cytoscape.
+
+    Returns dictionary with "networks" and "views".
+    """
+    if sbml_path.suffix != ".xml":
+        console.error(f"SBML path {sbml_path} does not have .xml extension")
 
     try:
         console.print(p4c.cytoscape_version_info())
@@ -39,10 +46,77 @@ def visualize_sbml(sbml_path: Path, delete_session: bool = False) -> None:
         if delete_session:
             p4c.session.close_session(save_before_closing=False)
 
-        p4c.networks.import_network_from_file(str(sbml_path))
+        networks_views = p4c.networks.import_network_from_file(str(sbml_path))
+        console.print(f"{networks_views}")
+        network = networks_views["networks"][1]
+        p4c.set_current_view(network=network)  # set the base network
+        return network
+
+        return networks_views
 
     except RequestException:
         logger.error(
             "Could not connect to a running Cytoscape instance. "
             "Start Cytoscape before running the python script."
         )
+        return None
+
+
+
+
+
+
+
+def read_layout_xml(sbml_path: Path, xml_path: Path) -> pd.DataFrame:
+    """Read own xml layout information form cytoscape."""
+    # read positions
+    df: pd.DataFrame = pd.read_xml(xml_path, xpath="//boundingBox")
+    df = df[['id', 'xpos', 'ypos']]
+    df.rename(columns={"xpos": "x", "ypos": "y"}, inplace=True)
+    df.set_index("id", inplace=True)
+    return df
+
+def apply_layout(network, layout: pd.DataFrame) -> None:
+    """Apply layout information from Cytoscape to SBML networks."""
+
+    # get SUIDs, sbml_id from node table;
+    df_nodes = p4c.get_table_columns(table="node", columns=["sbml id"], network=network)
+    console.print(df_nodes)
+    sid2suid = {row["sbml id"]: suid for suid, row in df_nodes.iterrows()}
+    console.print(sid2suid)
+
+    # FIXME: necessary to check that all sids exist
+    suids = [sid2suid[sid] for sid in layout.index.values]
+    x_values = layout["x"].values.tolist()
+    y_values = layout["y"].values.tolist()
+
+    # set positions
+    # see: https://github.com/cytoscape/py4cytoscape/issues/144
+    p4c.set_node_position_bypass(suids, new_x_locations=x_values, new_y_locations=y_values, network=network)
+    # p4c.set_node_property_bypass(suids, new_values=x_values, visual_property='NODE_X_LOCATION', network=network)
+    # p4c.set_node_property_bypass(suids, new_values=y_values, visual_property='NODE_Y_LOCATION', network=network)
+    p4c.set_current_view(network=network)
+
+    # remove bypass
+    # p4c.clear_node_property_bypass(suids, visual_property='NODE_X_LOCATION', network=network)
+    # p4c.clear_node_property_bypass(suids, visual_property='NODE_Y_LOCATION', network=network)
+
+    # fit content
+    p4c.fit_content()
+
+
+
+if __name__ == "__main__":
+    pass
+    # # visual style
+    # p4c.set_visual_style('Marquee')
+    #
+    # # fit the content
+    # p4c.fit_content()
+
+    # p4c.load_table_data
+
+    # annotations!
+
+    # network_views.export_image
+
