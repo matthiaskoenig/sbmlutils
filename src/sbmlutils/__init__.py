21cd0526d2757890e9b2fde2999597bd9d2f53bf
Matthias Koenig <konigmatt@googlemail.com>
2026-09-07 23:54:28 +0200

    Bump version: 0.10.0 → 0.10.1

--- src/sbmlutils/__init__.py
+++ src/sbmlutils/__init__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Matthias König"
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 
 program_name = "sbmlutils"
